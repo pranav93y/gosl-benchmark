68261e4e1c42aa4ec3decfee093d467f358644a1
Pranav Yerabati <pyerabat@ucsc.edu>
2018-03-11 09:47:47 -0700

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 import os
 
-print(os.environ['DIV'])
-print("test print")
+div = os.environ['DIV']
+threads = os.environ['THREAD']
 
 
+os.system("go build sparse.go")
+
+if threads == 1:
+	os.system("./sparse "+ str(div))	
+
+elif threads == 2:
+	str = "parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads))
+	os.system("parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)))
+	print str
+
+elif threads == 4:
+	str = "parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads))
+	os.system("parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)))
+	print str
+
+elif threads == 8:
+	str = "parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads))								
+	os.system("parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)))
+	print str
+else:
+	print "error: incorrect input"
+
+
+return
