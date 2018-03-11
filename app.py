5968baf83f4fdbfb0b115eec1ae5785197169383
Pranav Yerabati <pyerabat@ucsc.edu>
2018-03-11 10:55:28 -0700

    app.py update

--- app.py
+++ app.py
@@ -3,38 +3,29 @@ import os
 div = os.environ['DIV']
 threads = os.environ['THREAD']
 
-print div
-print type(div)
-
-print threads
-print type(threads)
-
-
 
 os.system("go build sparse.go")
 
 r = int(threads)*int(div)
 
-print r
-print type(r)
 
 if int(threads) == 1:
 	os.system("./sparse "+ str(r))	
 
 elif int(threads) == 2:
 	str = 'parallel ::: "./sparse '+ str(r)+'"'+' "./sparse '+str(r)+'"'
-	print str
+	#print str
 	os.system(str)
 	
 
 elif int(threads) == 4:
 	str = 'parallel ::: "./sparse ' + str(r)+'"'+' "./sparse '+str(r)+'"' + ' "./sparse '+str(r)+'"' + ' "./sparse '+str(r)+'"'
-	print str
+	#print str
 	os.system(str)
 
 elif int(threads) == 8:
 	str = 'parallel ::: " ./sparse ' + str(r)+'"'+' "./sparse '+str(r)+'"' + ' "./sparse '+str(r)+'"' + ' "./sparse '+str(r)+'"' + ' "./sparse ' + str(r)+'"'+' "./sparse '+str(r)+'"' + ' "./sparse '+str(r)+'"' + ' "./sparse '+str(r)+'"'								
-	print str
+	#print str
 	os.system(str)
 else:
 	print "error: incorrect input"
