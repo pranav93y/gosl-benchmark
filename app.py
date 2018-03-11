ee555e9f638861c0dfa7afcb5e00424befc7bca2
Pranav Yerabati <pyerabat@ucsc.edu>
2018-03-11 10:03:16 -0700

    app.py update

--- app.py
+++ app.py
@@ -6,22 +6,24 @@ threads = os.environ['THREAD']
 
 os.system("go build sparse.go")
 
+r = int(threads)*int(div)
+
 if int(threads) == 1:
 	os.system("./sparse "+ str(div))	
 
 elif int(threads) == 2:
-	str = "parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads))
-	os.system("parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)))
+	str = "parallel ::: ./sparse " + str(r)+" ./sparse "+str(r)
+	os.system("parallel ::: ./sparse " + str(r)+" ./sparse "+str(r))
 	print str
 
 elif int(threads) == 4:
-	str = "parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads))
-	os.system("parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)))
+	str = "parallel ::: ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r)
+	os.system("parallel ::: ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r))
 	print str
 
 elif int(threads) == 8:
-	str = "parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads))								
-	os.system("parallel ::: ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse " + str(int(div)*int(threads))+" ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)) + " ./sparse "+str(int(div)*int(threads)))
+	str = "parallel ::: ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r)								
+	os.system("parallel ::: ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r))
 	print str
 else:
 	print "error: incorrect input"
