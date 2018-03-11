651a01c8298163b5e7177244dcd2bb14b3a09a90
Pranav Yerabati <pyerabat@ucsc.edu>
2018-03-11 10:23:46 -0700

    app.py update

--- app.py
+++ app.py
@@ -23,18 +23,19 @@ if int(threads) == 1:
 
 elif int(threads) == 2:
 	str = "parallel ::: ./sparse " + str(r)+" ./sparse "+str(r)
-	os.system("parallel ::: ./sparse " + str(r)+" ./sparse "+str(r))
 	print str
+	os.system(str)
+	
 
 elif int(threads) == 4:
 	str = "parallel ::: ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r)
-	os.system("parallel ::: ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r))
 	print str
+	os.system(str)
 
 elif int(threads) == 8:
 	str = "parallel ::: ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r)								
-	os.system("parallel ::: ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse " + str(r)+" ./sparse "+str(r) + " ./sparse "+str(r) + " ./sparse "+str(r))
 	print str
+	os.system(str)
 else:
 	print "error: incorrect input"
 	print "threads: " + str(threads)
